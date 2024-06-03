070f04314057eba7ce7e048fafd6cd0a866644bc
engwellingtonpeb <wellington@peb.ufrj.br>
2024-06-03 15:20:22 -0300

    sw v5.0

---
+++ 07_TremorModel_v5/main.m
@@ -0,0 +1,73 @@
+%=========================================================================%
+%                   Federal University of Rio de Janeiro                  %
+%                  Biomedical Engineering Program - COPPE                 %
+%                   https://www.peb.ufrj.br/index.php/pt/                 %
+%                                                                         %
+% Advisor: Prof. Dr. Luciano L. Menegaldo                                 %
+% Doctoral Candidate: Wellington C. Pinheiro MSc.                         %
+%                                                                         %
+% v5 - Individualized tremor model and simulation scripts.                %
+%      The model is parametrized using GA optimization, electrical        %
+%      stimulation dynamics was embbeded and it can be used as            %
+%      environment for RL agent training. RL agent will control           %
+%      electrical stimulator                                              %
+%=========================================================================%
+
+clc
+clear all
+close all hidden
+pathconfig
+import org.opensim.modeling.*
+
+prompt="Select an operation mode:\n" + ...
+    "(1) - Individualized Tremor Model Tuning \n" + ...
+    "(2) - Individualized Tremor Model Simulation \n" + ...
+    "(3) - O.L e-stim Simulation \n" + ...
+    "(4) - e-stim RL controller Training \n" + ...
+    "(5) - e-stim MPC/RL controller design \n \n" + ...
+    "Option:";
+
+opt=input(prompt);
+
+
+switch opt
+
+    case 1
+        % get patient data
+
+        % get Optimization Routine to run
+
+        % save results
+
+
+    case 2
+        % get patient parameters or use a dummy parameter vector
+        getSimulationParams;
+
+        % run only tremor simulation without ES inputs
+        runIndividualizedTremorSimulation;
+
+    case 3
+
+        % run only tremor simulation without ES inputs
+        runIndividualizedTremorSimulationwithES;
+
+    case 4
+
+        %get patient parameters or use a dummy parameter vector
+        getSimulationParams;
+
+        %RL Controller Trainning
+        runRLControllerTrainning
+
+
+
+    case 5
+
+        disp('Module still being developed...')
+        disp('   ')
+
+end
+
+disp("That's all folks!")
+disp('PEB/COPPE/UFRJ')
---
+++ 07_TremorModel_v5/pathconfig.m
@@ -0,0 +1,54 @@
+%=========================================================================%
+%                   Federal University of Rio de Janeiro                  %
+%                  Biomedical Engineering Program - COPPE                 %
+%                   https://www.peb.ufrj.br/index.php/pt/                 %
+%                                                                         %
+% Advisor: Prof. Dr. Luciano L. Menegaldo                                 %
+% Doctoral Candidate: Wellington C. Pinheiro MSc.                         %
+%                                                                         %
+% Set paths and configurations to run on different PCs the FD simulations %
+%                                                                         %
+%=========================================================================%
+
+
+clear all
+clc
+close all
+
+import org.opensim.modeling.*
+
+name = getenv('COMPUTERNAME'); 
+
+
+if strcmp(name,'ENGWELLPC')
+
+    addpath('Tuning_Feature\')
+    addpath('\Users\engwe\Desktop\DSc_v4\02_Coletas')
+    addpath('\Users\engwe\Desktop\DSc_v4\03_ODE_Solvers')
+    addpath('\Users\engwe\Desktop\DSc_v4\04_DMDc_IDTF\simulations')
+    addpath('C:\Users\engwe\anaconda3\envs\mat_py')
+    
+    osimModel=Model('C:\Users\engwe\Desktop\DSc_v4\01_ModelFilesOsim41\MoBL-ARMS Upper Extremity Model\Benchmarking Simulations\4.1 Model with Millard-Schutte Matched Curves\MOBL_ARMS_module2_4_allmuscles_ignoreactivation.osim');
+    
+
+elseif strcmp(name,'MARCOPOLO')
+
+    addpath('..\Tuning_Feature')
+    addpath('\Users\Wellington\Desktop\DSc_v4\02_Coletas\')
+    addpath('\Users\Wellington\Desktop\DSc_v4\03_ODE_Solvers\')
+    addpath('\Users\Wellington\Desktop\DSc_v4\04_DMDc_IDTF\simulations\')
+    addpath('C:\Users\Wellington\.conda\envs\mat_py\')
+
+    osimModel=Model('C:\Users\Wellington\Desktop\DSc_v4\01_ModelFilesOsim41\MoBL-ARMS Upper Extremity Model\Benchmarking Simulations\4.1 Model with Millard-Schutte Matched Curves\MOBL_ARMS_module2_4_allmuscles_ignoreactivation.osim');
+    
+
+elseif strcmp(name,'ENGWELLSURFACE')
+
+    addpath('..\Tuning_Feature')
+    addpath('\02_DSc_v4\02_Coletas\')
+    addpath('\02_DSc_v4\03_ODE_Solvers\')
+    addpath('\02_DSc_v4\04_DMDc_IDTF\simulations\')
+
+    osimModel=Model('D:\02_DSc_v4\01_ModelFilesOsim41\MoBL-ARMS Upper Extremity Model\Benchmarking Simulations\4.1 Model with Millard-Schutte Matched Curves\MOBL_ARMS_module2_4_allmuscles_ignoreactivation.osim');
+    
+end
